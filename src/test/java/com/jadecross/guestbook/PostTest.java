7b0796a46ff0bbbce8361025860df67504f9180b
ofmw <ofmw91@gmail.com>
2024-03-13 14:04:42 +0900

    Delete Gap

--- src/test/java/com/jadecross/guestbook/PostTest.java
+++ src/test/java/com/jadecross/guestbook/PostTest.java
@@ -1,7 +1,7 @@
 package com.jadecross.guestbook;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import  org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
 class PostTest {
     @Test
